e81086abab27694cc804d189ad9d5346d38671f8
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-20 13:59:19 +0530

    udpate: cleaned the code for deploy.js

--- deploy.js
+++ deploy.js
@@ -2,7 +2,6 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
-// configuring a provider for unlocking an account and connecting to an external API
 const provider = HDWalletProvider(
   process.env.SECRET_PHRASE,
   "https://rinkeby.infura.io/v3/c65de9dd9e6a4d2cb17c756fb3f6a446"
