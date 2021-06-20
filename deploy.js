5039a6fb00a93b48e72b2e6cb509b457f282f252
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-20 22:27:03 +0530

    add: dotenv package added and fixed the provider setting in the deploy.js file

--- deploy.js
+++ deploy.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
-const provider = HDWalletProvider(
+const provider = new HDWalletProvider(
   process.env.SECRET_PHRASE,
   "https://rinkeby.infura.io/v3/c65de9dd9e6a4d2cb17c756fb3f6a446"
 );
@@ -11,14 +12,17 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
+  console.log(accounts);
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
-      data: bytecode,
+      data: `0x${bytecode}`,
     })
     .send({
       from: accounts[0],
       gas: "1000000",
+      gasPrice: web3.utils.toWei("2", "gwei"),
     });
+  console.log(interface);
   console.log("Contract deployed to ", result.options.address);
 };
 
